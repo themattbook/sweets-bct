1484d9dc006b0c7bf518de2db657e7758ef11dc7
themattbook <themattbook@gmail.com>
2015-05-17 09:43:04 -0500

    Create sweets-bct.py

---
+++ sweets-bct.py
@@ -0,0 +1,22 @@
+class pcolors:
+    HEADER = '\033[91m'
+    RETURN = '\033[0m'
+
+print pcolors.HEADER + "\nSweet's Binary Conversion Tool v1.0"
+print pcolors.RETURN + "Easily convert characters to Binary."
+print "Matthew Sweet - themattbook@gmail.com\n"
+
+print pcolors.HEADER + "\tWarning:",
+print pcolors.RETURN + """
+\tThis tool will not differentiate spacing. For example,
+\ttyping "Rock on" will also include the space between
+\tRock and on. A more accurate conversion would be "Rockon".
+"""
+
+print pcolors.RETURN + "Enter characters to convert: ",
+
+data = raw_input()
+
+print "\nResults: ",
+print pcolors.HEADER + (' '.join(format(ord(x), 'b') for x in data))
+print pcolors.RETURN
